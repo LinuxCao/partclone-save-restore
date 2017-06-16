8092c569781e3ded60e5917e9c10942ee442d954
caolinfeng <527121128@qq.com>
2017-06-16 17:53:34 +0800

    update

--- partclone-0.3.6/partclone_resotre.sh
+++ partclone-0.3.6/partclone_resotre.sh
@@ -7,6 +7,6 @@ echo
 
 echo "restore partition boot in partclone restore from "/mnt/image_directory/boot_sda1.img""
 umount /dev/sda1 
-./src/partclone.extfs -r -d -s /mnt/image_directory/boot_sda1.img -o /dev/sda1
+./partclone.extfs -r -d -s /mnt/image_directory/boot_sda1.img -o /dev/sda1
 mount /dev/sda1 /boot
 exit
--- partclone-0.3.6/partclone_save.sh
+++ partclone-0.3.6/partclone_save.sh
@@ -9,6 +9,6 @@ echo "backup partition boot in partclone save to "/mnt/image_directory/boot_sda1
 umount /dev/sda1 
 rm -fr /mnt/image_directory/boot_sda1.img
 mkdir -p /mnt/image_directory
-./src/partclone.extfs -c -d -s /dev/sda1 -o /mnt/image_directory/boot_sda1.img
+./partclone.extfs -c -d -s /dev/sda1 -o /mnt/image_directory/boot_sda1.img
 mount /dev/sda1 /boot
 exit
