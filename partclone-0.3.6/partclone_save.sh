bbb9bdb61019a625bae437987707f966cd52cadc
caolinfeng <527121128@qq.com>
2017-06-20 18:03:54 +0800

    gzip -c -1 root_partition_sda2.img > root_partition_sda2.img.gz

--- partclone-0.3.6/partclone_save.sh
+++ partclone-0.3.6/partclone_save.sh
@@ -13,9 +13,10 @@ mkdir -p /mnt/image_directory
 echo "backup partition / in partclone save to "/mnt/image_directory/root_partition_sda2.img""
 rm -fr /mnt/image_directory/root_partition_sda2.img
 ./partclone.extfs -c -d -s /dev/sda2 -o /mnt/image_directory/root_partition_sda2.img
+gzip -c -1 /mnt/image_directory/root_partition_sda2.img > /mnt/image_directory/root_partition_sda2.img.gz
 
-echo "backup partition /opt/ssd in partclone save to "/mnt/image_directory/opt_ssd_sda3.img""
-rm -fr /mnt/image_directory/opt_ssd_sda3.img
-./partclone.extfs -c -d -s /dev/sda3 -o /mnt/image_directory/opt_ssd_sda3.img
+#echo "backup partition /opt/ssd in partclone save to "/mnt/image_directory/opt_ssd_sda3.img""
+#rm -fr /mnt/image_directory/opt_ssd_sda3.img
+#./partclone.extfs -c -d -s /dev/sda3 -o /mnt/image_directory/opt_ssd_sda3.img
 
 exit
