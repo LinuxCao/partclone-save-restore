9d699dd0f9c4bd0b39dbc9ff3341b9f91637cdb4
caolinfeng <527121128@qq.com>
2017-07-04 10:14:52 +0800

    cancel gzip img when partclone_save

--- partclone-0.3.6/partclone_restore.sh
+++ partclone-0.3.6/partclone_restore.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 clear
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-echo "                        Welcome to partclone save os                         "
+echo "                        Welcome to partclone restore os                         "
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 echo
 
 echo "restore partition boot in partclone restore from "/mnt/image_directory/boot_sda1.img""
-./partclone.extfs -r -d -s /mnt/image_directory/boot_sda1.img -o /dev/sda1
+partclone.extfs -r -d -s /mnt/image_directory/boot_sda1.img -o /dev/sda1
 
 echo "restore partition root in partclone restore from "/mnt/image_directory/root_partition_sda2.img""
-gunzip -c /mnt/image_directory/root_partition_sda2.img.gz > /mnt/image_directory/root_partition_sda2.img
-./partclone.extfs -r -d -s /mnt/image_directory/root_partition_sda2.img -o /dev/sda2
+#gunzip -c /mnt/image_directory/root_partition_sda2.img.gz > /mnt/image_directory/root_partition_sda2.img
+partclone.extfs -r -d -s /mnt/image_directory/root_partition_sda2.img -o /dev/sda2
 
 #echo "restore partition /opt/ssd in partclone restore from "/mnt/image_directory/opt_ssd_sda3.img""
 #./partclone.extfs -r -d -s /mnt/image_directory/opt_ssd_sda3.img -o /dev/sda3
