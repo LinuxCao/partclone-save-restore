c0d3d5571a1d1e4b899ffce489b6c8fc1866e255
caolinfeng <527121128@qq.com>
2017-07-07 15:24:48 +0800

    comment swap UUID but /dev/sdaX table within etc/fstab because of reboot problem

--- partclone-0.3.6/partclone_restore_centos.sh
+++ partclone-0.3.6/partclone_restore_centos.sh
@@ -5,9 +5,17 @@ echo "                        Welcome to partclone restore centos os            
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 echo
 
+# restore sda sdb sdc partition use of partclone.extfs command
 partclone.extfs -r -d -s /mnt/image_directory/centos_boot_sda1.img -o /dev/sda1
 
 partclone.extfs -r -d -s /mnt/image_directory/centos_root_sda2.img -o /dev/sda2
 
+# comment swap UUID but /dev/sdaX table within etc/fstab because of reboot problem
+mount /dev/sda2 /mnt
+mount /dev/sda1 /mnt/boot
+sed -i "s/UUID=.* swap/#UUID=.* swap/g" /mnt/etc/fstab
+echo "/dev/sda3 swap swap defaults 0 0" >> /mnt/etc/fstab
+umount /mnt/boot
+umount /mnt
 
 exit
