a262920df26cb42e4c00ef23586aeb7af6218978
caolinfeng <527121128@qq.com>
2017-07-11 17:50:25 +0800

    add partclone.desktop into /usr/share/applications

--- partclone-0.3.6/partclone_save_centos.sh
+++ partclone-0.3.6/partclone_save_centos.sh
@@ -7,9 +7,11 @@ echo
 
 rm -fr /mnt/image_directory/centos_boot_sda1.img
 mkdir -p /mnt/image_directory
+fsck /dev/sda1
 partclone.extfs -c -d -s /dev/sda1 -o /mnt/image_directory/centos_boot_sda1.img
 
 rm -fr /mnt/image_directory/centos_root_sda2.img
+fsck /dev/sda2
 partclone.extfs -c -d -s /dev/sda2 -o /mnt/image_directory/centos_root_sda2.img
 
 
--- partclone-0.3.6/partclone_save_centos_fedora.sh
+++ partclone-0.3.6/partclone_save_centos_fedora.sh
@@ -7,23 +7,29 @@ echo
 
 rm -fr /run/media/liveuser/Fedora-23/mnt/image_directory/centos_boot_sda1.img
 mkdir -p /run/media/liveuser/Fedora-23/mnt/image_directory
+fsck /dev/sda1
 partclone.extfs -c -d -s /dev/sda1 -o /run/media/liveuser/Fedora-23/mnt/image_directory/centos_boot_sda1.img
 
 
 rm -fr /run/media/liveuser/Fedora-23/mnt/image_directory/centos_root_sda2.img
+fsck /dev/sda2
 partclone.extfs -c -d -s /dev/sda2 -o /run/media/liveuser/Fedora-23/mnt/image_directory/centos_root_sda2.img
 
 rm -fr /run/media/liveuser/Fedora-23/mnt/image_directory/centos_opt_sata_sdb1.img
+fsck /dev/sdb1
 partclone.extfs -c -d -s /dev/sdb1 -o /run/media/liveuser/Fedora-23/mnt/image_directory/centos_opt_sata_sdb1.img
 
 rm -fr /run/media/liveuser/Fedora-23/mnt/image_directory/centos_opt_ssd_sdc1.img
+fsck /dev/sdc1
 partclone.extfs -c -d -s /dev/sdc1 -o /run/media/liveuser/Fedora-23/mnt/image_directory/centos_opt_ssd_sdc1.img
 
 rm -fr /run/media/liveuser/Fedora-23/mnt/image_directory/fedora_boot_sda5.img
+fsck /dev/sda5
 partclone.extfs -c -d -s /dev/sda5 -o /run/media/liveuser/Fedora-23/mnt/image_directory/fedora_boot_sda5.img
 
 
 rm -fr /run/media/liveuser/Fedora-23/mnt/image_directory/fedora_root_sda6.img
+fsck /dev/sda6
 partclone.extfs -c -d -s /dev/sda6 -o /run/media/liveuser/Fedora-23/mnt/image_directory/fedora_root_sda6.img
 
 exit
