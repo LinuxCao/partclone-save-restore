aaaaf68027e7bf028bd69b33668dcc8980a299bb
caolinfeng <527121128@qq.com>
2017-07-13 08:47:55 +0800

    add centos_etc_fstab and fedora_etc_fstab

--- partclone-0.3.6/partclone_restore_centos.sh
+++ partclone-0.3.6/partclone_restore_centos.sh
@@ -13,8 +13,9 @@ partclone.extfs -r -d -s /mnt/image_directory/centos_root_sda2.img -o /dev/sda2
 # comment swap UUID but /dev/sdaX table within etc/fstab because of reboot problem
 mount /dev/sda2 /mnt
 mount /dev/sda1 /mnt/boot
-sed -i "s/UUID=.* swap/#UUID=.* swap/g" /mnt/etc/fstab
-echo "/dev/sda3 swap swap defaults 0 0" >> /mnt/etc/fstab
+#sed -i "s/UUID=.* swap/#UUID=.* swap/g" /mnt/etc/fstab
+#echo "/dev/sda3 swap swap defaults 0 0" >> /mnt/etc/fstab
+cp -f /opt/partclone-0.3.6/centos_etc_fstab /mnt/etc/fstab
 umount /mnt/boot
 umount /mnt
 
